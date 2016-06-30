da75d38877196bf848a0f32bee505aa93f5c526f
iamcdxn <iamcdxn@gmail.com>
2016-06-30 21:26:09 +0800

    測試測試
    這樣會怎樣呢？

---
+++ 0624討拍拍/LikedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LikedViewController.swift
+//  0624討拍拍
+//
+//  Created by CdxN on 2016/6/24.
+//  Copyright © 2016年 Uxpa_Everyday. All rights reserved.
+//
+
+import UIKit
+
+class LikedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
